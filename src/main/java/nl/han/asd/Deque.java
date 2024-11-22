8a539478537af3362c6c9cbc24381d9c2b377ee5
Reem2810 <84314460+Reem2810@users.noreply.github.com>
2024-11-22 22:10:55 +0100

    fixing pom

--- src/main/java/nl/han/asd/Deque.java
+++ src/main/java/nl/han/asd/Deque.java
@@ -6,36 +6,44 @@ public class Deque<T> {
 
 
     public Deque() {
+
         this.list = new DoubleLinkedList<>();
     }
 
 
     public void InsertLeft(T element) {
+
         list.addFirst(element);
     }
 
     public void InsertRight(T element) {
+
         list.addLast(element);
     }
 
     public T DeleteLeft() {
+
         return list.removeFirst();
     }
 
     public T DeleteRight() {
+
         return list.removeLast();
     }
 
     public int size() {
+
         return list.size();
     }
 
     public boolean isEmpty() {
+
         return list.isEmpty();
     }
 
     @Override
     public String toString() {
+
         return list.toString();
     }
 }
